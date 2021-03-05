bed4e764209d98f7c5b4af4d6378b6d5fa6f170c
Lucifer293 <80107371+Lucifer293@users.noreply.github.com>
2021-03-05 21:22:37 +0200

    Create Skip-Links.py

---
+++ Skip-Links.py
@@ -0,0 +1,26 @@
+red="\033[31m"
+yellow="\033[33m"
+import os
+import requests
+os.system("clear")
+#insta = 7.7.6.6.1
+#Amino = http://aminoapps.com/p/ovc8hb
+#YouTube = Lucifer 
+#Programmer = Lucifer 
+#=============================
+print("\033[31m")
+print("     \_______/")
+print(" `.,-'\_____/`-.,'")
+print("  /`..'\ _ /`.,'\ ")
+print(" /  /`.,' `.,'\  \ ")
+print("/__/__/     \__\__\__")
+print("\  \  \     /  /  /")
+print(" \  \,'`._,'`./  /")
+print("  \,'`./___\,'`./")
+print(" ,'`-./_____\,-'`.")
+print("     /       \ ")
+print("\033[33m")
+print("\n")
+print("open nano put Link ")
+L = requests.get("Link")
+print(L.url)
